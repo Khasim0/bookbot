e5e29f51c109e9508bdb26e4b1a01859c1ef3b76
Khasim Ali <mohammedkhasimali123@gmail.com>
2026-03-09 17:56:41 +0000

    day 3: bookbot project started

---
+++ main.py
@@ -0,0 +1,11 @@
+def get_book_text(filepath):
+    with open(filepath) as f:
+        contents = f.read()
+    return contents
+def main():
+    result = get_book_text("books/frankenstein.txt")
+    print (result)
+
+main()
+
+
