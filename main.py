aa1bf83e7aa9365ca3b329bc65b8171df6f64700
Khasim Ali <mohammedkhasimali123@gmail.com>
2026-03-12 14:39:22 +0000

    day 5: bookbot progress

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-
+from stats import get_num_words, get_num_char, get_char_counts
+import sys
 def get_book_text(filepath):
     with open(filepath) as f:
         contents = f.read()
     return contents
 
-def get_num_words(string):
-    words = string.split()
-    num_words = 0   
-    for word in words:
-        num_words += 1
-    
-    return num_words 
+
 
 def main():
-    result = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print ("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    result = get_book_text(sys.argv[1])
     
     num_words = get_num_words(result)
-    print (f"(Found {num_words} total words)")  
-main()
+    num_char = get_num_char(result)
+    char_count = get_char_counts(num_words, num_char)
+    print (f"Found {num_words} total words")  
+    
+    for item in char_count:
+        print(f"{item['char']}: {item['num']}")
 
+
+main()
 
--- stats.py
+++ stats.py
@@ -1,19 +1,32 @@
-from main import get_book_text
 def get_num_words(string):
-    words = string.spilt()
-    num_words = 0
+    words = string.split()
+    num_words = 0   
     for word in words:
-        num_words += word
-    return words 
-print (f"(Found {num_words} total words)")
+        num_words += 1
+    
+    return num_words 
 
-# in -  new funtion get text  from book as string. import from main
-# out - return number of time each character(symbol and space as well), return dict of string: integer
-# rule - 
-
-def get_num_words(filepath):
+def get_num_char(filepath):
     char= filepath.lower()
     num_char = {}
     for c in char:
-        num_char = num_words
-print num_char
+        if c in num_char:
+            num_char[c] += 1
+        else:
+            num_char[c] = 1
+        
+    return num_char
+
+def sort_on(items):
+        return items ['num']
+
+def get_char_counts( num_words, num_char ):
+    sorted_list =[]
+
+    for key, value in num_char.items():
+        sorted_list.append({"char": key, "num": value}) 
+    sorted_list.sort(reverse=True, key = sort_on)
+
+    return sorted_list
+    
+        
